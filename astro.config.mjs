7c3bc4b45c198e708a1453a7a910662094dddea7
lunora451 <itthings451@gmail.com>
2025-09-14 10:24:55 +0200

    oijo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   output: 'server',
+
   prefetch: {
     prefetchAll: true,
   },
+
   image: {
     responsiveStyles: true,
   },
+
+  adapter: netlify(),
 });
